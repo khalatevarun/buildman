e752bde9aed01f045d3b8f4b20ba3e49ec7a5657
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-12-19 11:50:53 -0800

    add base templates similar to bolt

---
+++ backend/src/constants.ts
@@ -0,0 +1,61 @@
+// see https://docs.anthropic.com/en/docs/about-claude/models
+export const MAX_TOKENS = 8192;
+
+// limits the number of model responses that can be returned in a single request
+export const MAX_RESPONSE_SEGMENTS = 2;
+
+export const WORK_DIR_NAME = 'project';
+export const WORK_DIR = `/home/${WORK_DIR_NAME}`;
+export const MODIFICATIONS_TAG_NAME = 'bolt_file_modifications';
+
+
+export const allowedHTMLElements = [
+    'a',
+    'b',
+    'blockquote',
+    'br',
+    'code',
+    'dd',
+    'del',
+    'details',
+    'div',
+    'dl',
+    'dt',
+    'em',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'hr',
+    'i',
+    'ins',
+    'kbd',
+    'li',
+    'ol',
+    'p',
+    'pre',
+    'q',
+    'rp',
+    'rt',
+    'ruby',
+    's',
+    'samp',
+    'source',
+    'span',
+    'strike',
+    'strong',
+    'sub',
+    'summary',
+    'sup',
+    'table',
+    'tbody',
+    'td',
+    'tfoot',
+    'th',
+    'thead',
+    'tr',
+    'ul',
+    'var',
+  ];
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
 import Anthropic from "@anthropic-ai/sdk";
+import { basePrompt } from "./defaults/react";
 
 const anthropic = new Anthropic();
 
 async function main() {
     anthropic.messages.stream({
-        messages: [{role: 'user', content: "create a simple todo app"}],
+        messages: [
+            {role: 'user', content: "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags."},
+            { role: 'user', content: basePrompt},
+            { role: 'user', content: "build a todo app"},
+        
+        
+        
+        ],
         model: 'claude-3-5-sonnet-20241022',
         max_tokens: 1024,
     }).on('text', (text) => {
